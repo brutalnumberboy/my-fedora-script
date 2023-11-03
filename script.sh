8e206cbe95d4dd5caec846c00bd9d8e5ba6d6318
brutalnumberboy <133921480+brutalnumberboy@users.noreply.github.com>
2023-11-03 17:04:21 +0100

    initial commit

---
+++ script.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+#my fedora installation script (works only with fedora workstation!!)
+#===============#
+#improve dnf performance
+echo 'max_parallel_downloads=10
+fastestmirror=True' | sudo tee -a /etc/dnf/dnf.conf > /dev/null
+#===============#
+
+#===============#
+#upgrade the system
+sudo dnf upgrade -y 
+#===============#
+
+#===============#
+#add rpmfusion repos
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf groupupdate core -y
+sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+sudo dnf groupupdate sound-and-video -y
+#===============#
+
+#===============#
+#install native packages 
+sudo dnf install akmods neofetch pandoc texlive-scheme-basic gnome-tweaks distrobox gnome-console -y
+#===============#
+
+#===============#
+#remove gnome-boxes and install virt-manager
+sudo dnf remove gnome-boxes -y
+sudo dnf install @Virtualization -y
+sudo systemctl enable libvirtd
+#===============#
+
+#===============#
+#remove native ff and libreoffice packages to replace them with flatpaks
+sudo dnf remove firefox *libreoffice* -y
+#===============#
+
+#===============#
+#swap nano to vim
+sudo dnf swap nano-default-editor vim-default-editor -y
+#===============#
+
+#===============#
+#import vscode repo and install code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+sudo dnf install code -y
+#===============#
+
+#===============#
+#install flatpaks
+flatpak update -y
+flatpak install flathub org.mozilla.firefox org.libreoffice.LibreOffice com.mattjakeman.ExtensionManager com.bitwarden.desktop com.github.tchx84.Flatseal com.usebottles.bottles de.haeckerfelix.Fragments org.telegram.desktop com.github.micahflee.torbrowser-launcher org.racket_lang.Racket io.mpv.Mpv com.spotify.Client com.discordapp.Discord -y
+#===============#
+
+#===============#
+#configure mpv
+flatpak run io.mpv.Mpv
+echo"profile=gpu-hq
+save-position-on-quit
+vo=gpu-next
+pause" > ~/.var/app/io.mpv.Mpv/config/mpv/mpv.conf
+#===============#
+
+#===============#
+#configure vim
+echo "set nocompatible
+filetype on
+filetype plugin indent on
+set title
+syntax on
+set number relativenumber
+set ts=4 sw=4" > ~/.vimrc
+#===============#
+
+#===============#
+#restart the system
+reboot
+#===============#
